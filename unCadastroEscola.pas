b01ba5414f118f2766de8ccf53aec513f46ef22a
Robson Paiva <paivawork@hotmail.com>
2018-12-03 11:12:36 -0300

    Início do projeto escola

---
+++ unCadastroEscola.pas
@@ -0,0 +1,24 @@
+unit unCadastroEscola;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TfrmCadastroEscola = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmCadastroEscola: TfrmCadastroEscola;
+
+implementation
+
+{$R *.dfm}
+
+end.
