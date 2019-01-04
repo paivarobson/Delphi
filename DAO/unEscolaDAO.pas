a1c8efd276704a6b4075e1492a0f09e88029f186
Robson Paiva <paivawork@hotmail.com>
2019-01-04 11:57:44 -0300

    Criação de unEscolaDAO e renomeado a EscolaModelo

---
+++ DAO/unEscolaDAO.pas
@@ -0,0 +1,23 @@
+unit unEscolaDAO;
+
+interface
+
+uses
+  unEscolaModelo;
+
+type
+  TEscolaDAO = class
+    procedure CarregarEscola(AEscolaModelo: TEscolaModelo);
+  end;
+
+implementation
+
+
+{ TEscolaDAO }
+
+procedure TEscolaDAO.CarregarEscola(AEscolaModelo: TEscolaModelo);
+begin
+
+end;
+
+end.
